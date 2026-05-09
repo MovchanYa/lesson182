129142680342a75de58628ecae5f1aaa1c0dd662
MovchanYa <0chanbanan0@gmail.com>
2026-05-10 00:26:30 +0300

    update configuration file

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    viewportWidth: 700,
+    viewportHeight: 720,
+    baseUrl: "https://youtube.com",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
